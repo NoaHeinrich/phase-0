0f39cb9684379a05f1fb888a43c60571798f0e98
Noah Heinrich <noahheinrich@gmail.com>
2015-09-29 08:53:27 -0500

    add basic math exercise

---
+++ week-4/basic-math.rb
@@ -0,0 +1,21 @@
+num1=9
+num2=25
+
+sum=num1+num2
+puts sum
+
+difference=num2-num1
+puts difference
+
+product=num1*num2
+puts product
+
+num1=9.0
+num2=25.0
+quotient=num2/num1
+puts quotient
+
+num1=9
+num2=25
+modulus=num2%num1
+puts modulus
